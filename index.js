c20b55674ee6153bce0e11d82fd4d9a3109a5c02
stio <dev@stio.studio>
2024-03-02 23:37:27 +0100

    http server

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 import { WebSocketServer } from 'ws';
+import * as http from 'http';
+import * as fs from 'fs';
+
+http.createServer(function (request, response) {
+    console.log(request.url)
+    switch (request.url) {
+        case "/":
+            fs.readFile("./client/chat.html", function (error, content) {
+                response.writeHead(200, { 'Content-Type': "text/html" });
+                response.end(content, 'utf-8');
+            });        
+            break;
+
+        case "/chat.js":
+            fs.readFile("./client/chat.js", function (error, content) {
+                response.writeHead(200, { 'Content-Type': "text/javascript" });
+                response.end(content, 'utf-8');
+            });        
+            break;
+
+        case "/style.css":
+            fs.readFile("./client/style.css", function (error, content) {
+                response.writeHead(200, { 'Content-Type': "text/css" });
+                response.end(content, 'utf-8');
+            });        
+            break;
+    
+        default:
+            response.statusCode = 404
+            response.end("404")
+            break;
+    }
+}).listen(5001);
 
 const wss = new WebSocketServer({ port: 5000 });
 
